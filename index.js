67e289c19eaf908ac5377d41afaefc5e8a2577bc
Eduardo Perez <a.eduardoperez.fp2019@gmail.com>
2021-10-15 14:48:46 -0400

    banco

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Escriba un algoritmo para cobrar un cheque genérico en un banco
-// VALIDAR QUE SE EL USUARIO CON UNA CONTRASE:A
-
 let money = parseInt(prompt('cuanto dinero tienes'));
 
 const remove = document.getElementById('remove')
@@ -8,30 +5,45 @@ const add = document.getElementById('add')
 alert(`tienes en la cuenta ${money}`)
 
 remove.addEventListener('click', ()=>{
-  let removeMoney = parseInt(prompt('Cuanto desea retirar'));
-  if(money >= removeMoney) {
-    alert(`Le quedan ${money - removeMoney}`);
-        console.log(removeMoney)
-  } else {
-    alert('no tienes plata suficiente jaja mmgvo pobre')
+  let passwordRemove = ()=> {
+    let passRemove = prompt('introduzca la clave')
+    if(passRemove == 'mundo') {
+
+      let removeMoney = parseInt(prompt('Cuanto desea retirar'));
+      if(money >= removeMoney) {
+        money -= removeMoney 
+        alert(`Le quedan ${money}`);
+      } else {
+        alert('no tienes plata suficiente jaja mmgvo pobre')
+      }
+    } else if (passRemove !== 'mundo') {
+      alert('la clave es incorrecta')
+      passwordRemove();
+    }
   }
+  passwordRemove()
 })
 
 add.addEventListener('click', ()=>{
-  let addMoney = parseInt(prompt('Cuanto desea añadir'));
-  if(addMoney > 3000) {
-    alert(`Usted no puede sacar mas 2999, no puede sacar ${addMoney}`);
-  } else if (addMoney < 100){
-    alert('no puedes sacar menos de 100')
-  } else {
-    alert('Gracias por usar el banco para meter plata')
-    money + addMoney
+  let passwordAdd = ()=> {
+    let passAdd = prompt('introduzca la clave') 
+    if(passAdd == 'mundo') {
+
+      let addMoney = parseInt(prompt('Cuanto desea añadir'));
+      if(addMoney > 3000) {
+        alert(`Usted no puede sacar mas 2999, no puede sacar ${addMoney}`);
+      } else if (addMoney < 100){
+        alert('no puedes sacar menos de 100')
+      } else {
+        alert('Gracias por usar el banco para meter plata')
+        money -= removeMoney 
+      }
+    } else if (passwordAdd !== 'mundo') {
+      alert('la clave es incorrecta')
+      passwordAdd();
+    }
   }
+  passwordAdd()
 })
 
-// condicional
-// let calc = (resOne, resTwo) => {
-//   return resOne - resTwo
-// }
-// calc(money, removeMoney)
-// console.log(calc)
+
